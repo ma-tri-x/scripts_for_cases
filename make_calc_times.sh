9b4d2a69b5b2f09b8357156e9f6b1004bfe8203e
ma-tri-x <niob@gmx.li>
2020-05-27 21:24:03 +0200

    make_calc_times.sh now should work with any cases

--- make_calc_times.sh
+++ make_calc_times.sh
@@ -7,8 +7,9 @@ echo "#cellsize  exec_time  number_of_cells" > calc_times.dat
 for i in ../${casename}*;do
     echo $i
     cd $i
-    cs=$(python getparm.py -q cellSize -s mesh)
-    exec_time=$(tail -n 40 run.log | grep Exec | tail -n 1 | sed "s/ExecutionTime = //g" | sed "s/ s//g")
+#     cs=$(python <<< "$(python getparm.py -q cellSize -s mesh)*1e6")
+    cs=$(m4 <<< "esyscmd(perl -e 'printf (  $(python getparm.py -q cellSize -s mesh)*1e6  )')")
+    exec_time=$(tail -n 60 run.log | grep Exec | tail -n 1 | sed "s/ExecutionTime = //g" | sed "s/ s//g")
     nc=$(grep nCells log.blockMesh | sed "s/  nCells: //g")
     if [[ ! $exec_time == "" ]];then
        echo "$cs  $exec_time  $nc" >> $thisdir/calc_times.dat
