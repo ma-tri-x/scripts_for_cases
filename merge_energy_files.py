de29bda537aabdde963e94be30dd6e55fdf7c7aa
ma-tri-x <niob@gmx.li>
2020-07-28 03:13:57 +0200

    update bubble energy to python scripted

--- merge_energy_files.py
+++ merge_energy_files.py
@@ -1,26 +1,116 @@
 #!/bin/python
 
-import os, sys, glob
+import os, sys, glob,json
 import numpy as np
 
+this_path = os.path.dirname(os.path.abspath( __file__ ))
+
+def getparm(subdict,query,json_file=os.path.join(this_path,"conf_dict.json")):
+    try:
+        with open(json_file,'r') as f:
+            conf_dict = json.load(f)
+    except(ValueError,IOError):
+        print "ERROR: parm not found"
+        exit(1)
+        
+    if subdict:
+        return conf_dict[subdict][query]
+    else:
+        return conf_dict[query]
+
+def radius(al):
+    Rs = (vol_convert(al.T[1])*3./4./np.pi)**(1./3.)
+    return Rs
+
+def get_time_of_Rn(al):
+    Rn = getparm("bubble","Rn")
+    Rs = radius(al)
+    index = np.argmin(np.abs(Rs-Rn))
+    timeOfRn = Rs[index]
+    return index,timeOfRn
+
+def vol_convert(energy):
+    dims = getparm("mesh","meshDims")
+    conv_fact = 1.0
+    theta = 0.
+    with open(os.path.join(this_path,"THETA"),'r') as g:
+        t=g.readlines()
+    theta = float(t[0].split('\n')[0])
+    if dims == "1D":
+        conv_fact = np.pi/np.tan(theta/180.*np.pi)**2
+    if dims == "2D":
+        conv_fact = 180./theta
+    return conv_fact*energy
+
+def PotEn(al,pg):
+    V = vol_convert(al.T[1])
+    V0= V[0]
+    pstat = getparm("liquid","pInf")
+    dV = []
+    pdV = 0.0
+    int_pdV = []
+    for i,entry in enumerate(V):
+        if i == 0: dV.append(0.0)
+        else:
+            dV.append(entry-V[i-1])
+        pdV = pg[i][1]*dV[i]
+        if i == 0: int_pdV.append(0.0)
+        else: int_pdV.append(int_pdV[-1]+pdV)
+    
+    indexRn,timeRn = get_time_of_Rn(al)
+    constant = int_pdV[indexRn]
+    potEn = -1*np.array(int_pdV) + constant + pstat*(V-V0)
+    Etot = potEn[0]
+    if not len(potEn) == len(al.T[0]):
+        print("coding error: len(potEn)={} len(al.T[0])={}".format(len(potEn),len(al.T[0])))
+        exit(1)
+    return potEn,Etot
+    
+    
+
 def main():
-    this_path = os.path.dirname(os.path.abspath( __file__ ))
     
     postProcessPath = os.path.join(this_path,"postProcessing/volumeIntegrate_volumeIntegral/0/")
+    alpha2File = os.path.join(postProcessPath, "alpha2")
     AcousticEnergyFile = os.path.join(postProcessPath, "AcousticEnergy")
+    AcousticEnergyGasFile = os.path.join(postProcessPath, "AcousticEnergyGas")
     KineticEnergyFile = os.path.join(postProcessPath, "KineticEnergy")
+    PotentialEnergyFile = os.path.join(postProcessPath, "PotentialEnergy")
     
+    workingPath=this_path
+    if os.path.isdir(os.path.join(this_path,"processor0")): 
+        workingPath = os.path.join(this_path,"processor0")
+    AvgPgFile = os.path.join(workingPath, "avg_pg.dat")
+    
+    al = np.loadtxt(alpha2File)
     Ac = np.loadtxt(AcousticEnergyFile)
+    AcG= np.loadtxt(AcousticEnergyGasFile)
     Ki = np.loadtxt(KineticEnergyFile)
+    pg = np.loadtxt(AvgPgFile)
     
+    alTime = al.T[0]
     AcTime = Ac.T[0]
+    AcGTime= AcG.T[0]
     KiTime = Ki.T[0]
+    pgTime = pg.T[0]
     
-    if AcTime.all() == KiTime.all():
+    if AcTime.all() == KiTime.all() \
+       and AcTime.all() == alTime.all() \
+       and AcTime.all() == AcGTime.all() \
+       and AcTime.all() == pgTime.all():
+        potEn,Etot = PotEn(al,pg)
+        radii = radius(al)
+        AC = vol_convert(Ac.T[1]) 
+        ACG= vol_convert(AcG.T[1])
+        KI = vol_convert(Ki.T[1]) 
+        PG = vol_convert(pg.T[1]) 
+        with open(os.path.join(this_path,"Etot"),"w") as f:
+            f.write("{}".format(Etot))
         with open(os.path.join(postProcessPath,"Energies"),"w") as f:
-            f.write("#time\t AcEnergy\t KinEnergy\n")
+            f.write("#time[mus]\t alpha2\t radius\t AcEnergy\t AcGasEnergy\t KinEnergy\t PotEnergy\n")
             for i,t in enumerate(AcTime):
-                f.write("{}\t{}\t{}\n".format(t,Ac[i][1],Ki[i][1]))
+                totEn = AC[i]+ACG[i]+KI[i]+potEn[i]
+                f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(t*1e6,al[i][1],radii[i],AC[i],ACG[i],KI[i],potEn[i],totEn))
     else:
         print("ERROR: times didn\'t match")
         exit(1)
--- plot_bubble_energy.gnuplot
+++ plot_bubble_energy.gnuplot
@@ -9,7 +9,8 @@ set key top center
 
 gamma=1.4
 pinf=101315.
-Vn=4./3.*pi*(0.000184103703538)**3
+Rn=0.000184103703538
+Vn=4./3.*pi*(Rn)**3
 theta=system("cat THETA")
 
 set xlabel "time [mus]"
@@ -26,26 +27,22 @@ V0=V(V0)
 pVgamma(x,y)= x*V(y)**gamma
 energy(x,y)=(x*V(y)-pinf*Vn)/(gamma-1.) + pinf*(V(y)-V0)
 energy2(x)=(pinf*Vn)/(gamma-1.) *((Vn/V(x))**(gamma-1.) - 1.) + pinf*(V(x)-V0)
-
-
-AcEn(x)=(pi/(tan(theta*pi/180.))**2)*x/(998.*1450**2)
-KinEn(x)=(pi/(tan(theta*pi/180.))**2)*x
-
-
+energy3(x)=V(x)<Vn?(pinf*Vn)/(gamma-1.) *((Vn/V(x))**(gamma-1.) - 1.) + pinf*(V(x)-V0):pinf*(V(x)-V0)
 R(x)=(3.*x/4./(tan(theta*pi/180.))**2)**(1./3.)
 
 pvgamma0=p0*V0**gamma
 Etot=(p0*V0-pinf*Vn)/(gamma-1)
+Etot2=system("cat Etot")
 
-print pvgamma0
-print Etot
-print Vn
-print V(2.0071213964765e-16)
-
+#1      2        3         4    5       6    7         8
+#t*1e6,al[i][1],radii[i],AC[i],ACG[i],KI[i],potEn[i],totEn
 p \
-  "postProcessing/volumeIntegrate_volumeIntegral/0/alpha2" u (($1)*1e6):( R($2)) w l lw 3 axes x1y2 t "R(t)" ,\
-  "postProcessing/volumeIntegrate_volumeIntegral/0/alpha2" u (($1)*1e6):( energy2(($2))/ Etot) w l lw 3 t "energy factor without avg pg",\
-  "postProcessing/volumeIntegrate_volumeIntegral/0/AcousticEnergy" u (($1)*1e6):( AcEn(($2))/ Etot) w l lw 3 t "Acoustic Energy",\
-  "postProcessing/volumeIntegrate_volumeIntegral/0/KineticEnergy" u (($1)*1e6):( KinEn(($2))/ Etot) w l lw 3 t "Kinetic Energy",\
+  "postProcessing/volumeIntegrate_volumeIntegral/0/Energies" \
+       u 1:(($3)*1e6) w l lw 3 axes x1y2 t "R(t)" ,\
+  ""   u 1:(($4)/ Etot) w l lw 3 t "Ac En. water",\
+  ""   u 1:(($6)/ Etot) w l lw 3 t "kin. En.",\
+  ""   u 1:(($7)/ Etot) w l lw 3 t "pot. En.",\
+  ""   u 1:(($5)/ Etot) w l lw 3 t "Ac En. bubble",\
   "incompressible_Rt.dat" u (($1)*1e6):($3) w l lw 3 lc 7 dt 3 t "incompressible energy"
   
+  #""   u 1:(($8)/ Etot) w l lw 2 t "total Energy",\
