3ef4de257f3513cb2a5b3139e616afade80d5c29
Max Koch <max.koch@phys.uni-goettingen.de>
2025-12-03 19:22:18 +0100

    added adapt_energy_LUT to OFAllFunctionLibrary.py

--- 2023_revolution_AllrunPython/OFAllFunctionLibrary.py
+++ 2023_revolution_AllrunPython/OFAllFunctionLibrary.py
@@ -434,6 +434,10 @@ class Case(object):
         return Rn_new    
     
     def adapt_energy(self):
+        print("---- ERROR: function adapt_energy is deprepaced. ----")
+        print("---- use adapt_energy_LUT instead ----")
+        exit(1)
+        
         print("---- setting pressure with same energy for discretization ----")
         p_init = self.pBubble # pBubble includes pV, pn doesn't
         Vn    = 4.*np.pi/3.* self.Rn**3  
@@ -479,6 +483,62 @@ class Case(object):
         #
         self.replace_variable_in_OF_dict("constant/transportProperties","gas","Rn",self.Rn)
         
+    def adapt_energy_LUT(self):
+        true_alpha2_vol = self.get_alpha2_vol_t0()
+        Vinitdiscr = true_alpha2_vol
+        
+        if self.conf_dict["mesh"]["meshDims"] == "2D":
+            print("... adapt_energy: calculating 2D volume")
+            theta = self.read_theta()
+            Vinitdiscr = true_alpha2_vol * 180. / theta
+        elif self.conf_dict["mesh"]["meshDims"] == "1D":
+            print("... adapt_energy: calculating 1D volume")
+            theta = self.read_theta()
+            theta_rad = theta * np.pi /180.
+            Vinitdiscr = true_alpha2_vol * np.pi / (np.tan(theta_rad))**2
+        
+        Rdiscr = (Vinitdiscr / (4.*np.pi)*3)**(1./3.)
+        print("... adapt_energy: R0_input = {}".format(self.R0))
+        print("... adapt_energy: Rdiscr   = {}".format(Rdiscr))
+        
+        Rmax_wanted = self.conf_dict["bubble"]["Rmax"]
+        
+        print("... adapt_energy: Rmax_want= {}".format(Rmax_wanted))
+        
+        R0_min = 10e-6
+        R0_max = 200e-6
+        Rn_min = 120e-6
+        Rn_max = 340e-6
+        if Rdiscr < R0_min or Rdiscr > R0_max:
+            print("ERROR in adapt_energy_LUT: Rdiscr < R0_min or Rdiscr > R0_max")
+            exit(1)
+            
+        #now finding Rn in LUT fitting to Rdiscr and Rmax
+        LUT = np.loadtxt("LUT_R0_Rn_Rmax.dat")
+        search_function = np.abs(LUT[:,0]-Rdiscr) + np.abs(LUT[:,2]-Rmax_wanted)
+        find_index = np.argmin(search_function)
+        R0_found = LUT[:,0][find_index]
+        Rn_found = LUT[:,1][find_index]
+        Rmax_found = LUT[:,2][find_index]
+        
+        print("... adapt_energy: found R0  ={}".format(R0_found))
+        print("... adapt_energy: found Rn  ={}".format(Rn_found))
+        print("... adapt_energy: found Rmax={}".format(Rmax_found))
+        
+        print("pBubble_old = {}".format(self.pBubble))
+        self.R0 = R0_found
+        self.Rn = Rn_found
+        self.pn = self.pInf + 2.*self.sigma/self.Rn - self.pV
+        self.pBubble = self.pn * ((self.Rn**3. - self.beta*self.Rn**3.)/(self.R0**3. - self.beta*self.Rn**3.))**self.gamma + self.pV 
+        print("pBubble_new = {}".format(self.pBubble))
+        
+        expression = f"{self.pBubble}*(1.-alpha1)+{self.pVar}*alpha1"
+        self.run_funkySetFields_command(self.pVar,expression,"")
+        #
+        self.replace_variable_in_OF_dict("constant/transportProperties","gas","Rn",self.Rn)
+        
+        
+            
     def replace_variable_in_OF_dict(self,OF_file,subdict,var_string,value):
         with open(OF_file,"r") as f:
             lines = f.readlines()
